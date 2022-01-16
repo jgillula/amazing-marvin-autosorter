f0a7047073d8691cd9ff223e588e3aeaad034749
Jeremy Gillula <jgillula+github@gmail.com>
2022-01-16 00:06:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Amazing Marvin Auto-Sorter
+
+A docker container you can run to automatically sort your [Amazing Marvin](https://amazingmarvin.com) tasks and projects every time they change.
+
+## How it works 
+
+Amazing Marvin offers a set of [webhooks](https://github.com/amazingmarvin/MarvinAPI/wiki/Webhooks) that can contact a server of your choosing every time a certain actions is performed. Amazing Marvin also provides direct access to the [underlying database where your tasks and projects are stored](https://github.com/amazingmarvin/MarvinAPI/wiki/Database-Access). Put those two together, and you can have Amazing Marvin post an HTTP request to your docker container when a change happens, and your docker container can use the database access to re-sort the tasks.
+
+## Why is it necessary?
+
+Unfortunately Amazing Marvin doesn't let you create persistent sorts based on whether or not a task is done, or whether or not it's on the backburner, or whether or not it has dependencies. This means even when you check off a task, it won't move to the bottom of your master list. It also means that if a task is blocked waiting on other tasks, there's no way to automatically move it to the bottom of your list.
+
+## Setup
+
+### 1. Setup and run the docker container
+
+### 2. Configure the docker container
+
+The docker container is configured via environment variables. The following environment variables are **required**:
+
+* `COUCHDB_USERNAME` - The username for the Amazing Marvin couchdb instance, available [here](https://app.amazingmarvin.com/pre?api) as `syncUser`
+* `COUCHDB_PASSWORD` - The password for the Amazing Marvin couchdb instance, available [here](https://app.amazingmarvin.com/pre?api) as `syncPassword`
+* `COUCHDB_HOSTNAME` - The hostname for the Amazing Marvin couchdb instance, available [here](https://app.amazingmarvin.com/pre?api) as `syncServer`
+* `COUCHDB_DATABASE` - The specific database within the Amazing Marvin couchdb instance, available [here](https://app.amazingmarvin.com/pre?api) as `syncDatabase`
+* `ACCESS_TOKENS_LIST` - This is a JSON list of strings, each of which is an access token that the docker container will use to authenticate the clients that connect to it. I recommend using something long and random, like the output of `import random, string; print(''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64)))`. It's OK if there is only one item in this list and you just use the same access token everywhere, but it's a list in case you want to have different clients use different access tokens.
+* `SORT_LIST` - A JSON list that descripbes the sort you want automatically applied after any changes. See [Sort list syntax](FIXME) for the syntax.
+
+The following environment variables are *optional*:
+
+* `UPDATE_TIMEOUT_MILLISECONDS` - When the docker container is triggered, it needs to make sure Amazing Marvin's backend database is updated before it should start sorting. This is how long the docker container will wait for the backend database to be updated before giving up. Default is 5000 milliseconds.
+* `QPS_RATE_LIMIT` - The rate limit in queries per second for queries from the docker container to the Amazing Marvin backend database. The nice folks at Amazing Marvin [ask that you not submit more than one query per second](https://github.com/amazingmarvin/MarvinAPI/wiki#rate-limits), so you should make this no greater than 1.0 (which is the default).
+
+### 3. Configure Amazing Marvin
+
+The next step is to configure Amazing Marvin.
+
+1. Since Amazing Marvin's webhooks can't be setup to operate on all tasks, we'll create a Smart List which will include all tasks. To do so, create a new smart list, name it whatever you want (e.g. "All Tasks"), and under "Filters" set "Item Type" to "Tasks". Then save.
+2. Go to the API feature settings, and add the following webhooks:
+**Trigger** | **Method** | **URL**
+---|---|---
+Edit|`POST`|`{DOCKER_URL}/edit`
+Add Task/Project|`POST`|`{DOCKER_URL}/add`
+Mark Done|`POST`|`{DOCKER_URL}/markDone`
+Delete|`POST`|`{DOCKER_URL}/delete`
+where `{DOCKER_URL}` is the URL of your docker container (e.g. `https://some.domain.com`). Set the Smart List for each webhook to the "All Tasks" list you created in step one. Finally, for **each** webhook set the headers to:
+```
+Authorization: Bearer {ACCESS_TOKEN}
+ContentType: application/json
+```
+where `{ACCESS_TOKEN}` is one of the access tokens you created back when you were configuring the docker container.
+3. Finally, if you change something and it doesn't trigger a sort, you can always trigger one manually by visiting `{DOCKER_URL}/{ACCESS_TOKEN}/sortAll` (where again, `{DOCKER_URL}` is the URL of your docker container and `{ACCESS_TOKEN}` is one of the access tokens you created). If the sort is successful, the page will automatically close itself as soon as the sort is done. As a neat trick, you can add this as an external link in your customizable sidebar (or bottom bar, or anywhere else Amazing Marvin lets you put an external link). That way you can trigger a manual sort directly from within Marvin.
+
+## Sort list syntax
+
+`SORT_LIST` should be a JSON list, where each item in the list is another JSON list where the first item is a string specifying the type of sort, and the second item is a dictionary specifying the options for that sort.
+
+The types of sorts and their options are:
+- `"field"` -- Sorts by a built-in Amazing Marvin field.
+ - `"field_name"` -- The name of the field as a string. Valid fields are [listed here](https://github.com/amazingmarvin/MarvinAPI/wiki/Marvin-Data-Types#tasks).
+ - `"empty_value"` -- The value to use when sorting if the field is not present in a task.
+ - `"reverse"` -- *Optional* Set to either `true` or `false` to control the order of the sort. Defaults to `false`.
+- `"is_ready"` -- Sorts by whether tasks are ready or not (i.e. not on the backburner, and not blocked by any dependencies that aren't done). By default, tasks that aren't ready appear before tasks that are (since `False==0` and `True==1` and `0 < 1`).
+ - `"reverse"` -- *Optional* Set to either `true` or `false` to control the order of the sort. Defaults to `false`.
+- `"label"`
+ - `"labels"` -- A list of label names as strings in the order you want to sort them.
+ - `"no_match_last"` -- *Optional* Set to `true` if you want tasks without the label to be sorted at the end, or `false` if you want them at the beginning. Defaults to `true`.
+ - `"reverse"` -- *Optional* Set to either `true` or `false` to control the order of the sort. Defaults to `false`.
+
+So for example, if we set `SORT_LIST` to
+```
+[
+    ["field", {"field_name": "done", 
+               "empty_value": false}],
+    ["is_ready", {"reverse": true}],
+    ["label", {"labels": ["In Progress", "Next", "Later", "Waiting"]}],
+    ["field", {"field_name": "isStarred", 
+    					 "empty_value": 0, 
+               "reverse": true}]
+]
+
+```
+Then the Amazing Marvin Autosorter would:
+1. Sort by the `done` field, treating tasks that don't have the `done` field as `False`. Note that `False` comes before `True`, so tasks where `done==False` will be above tasks that are done.
+2. Within the previous sort, sort tasks that are ready above tasks that are not (since `reverse` was `True`).
+3. Within the previous sort, sort tasks by the given labels, in that order, with tasks that don't have any of those labels at the end.
+4. Within the previous sort, sort tasks so that high priority tasks are at the top and low priority tasks are at the bottom (and tasks with no priority are at the end), since Amazing Marvin sets isStarred to a higher value the higher the priority and we reversed the sort.
---
+++ main.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import time
+
+# We use flask to handle the HTTP requests
+# https://flask.palletsprojects.com/en/2.0.x/
+from flask import Flask, request, send_from_directory
+
+# We use flask_cors because Amazing Marvin requires CORS
+# https://github.com/amazingmarvin/MarvinAPI/wiki/Webhooks#technical-details
+# https://flask-cors.readthedocs.io/en/latest/
+from flask_cors import CORS
+
+# We use flask_httpauth to restrict access to clients providing the correct HTTP token
+# https://flask-httpauth.readthedocs.io/en/latest/index.html
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication
+from flask_httpauth import HTTPTokenAuth
+
+# We use couchdb to access Amazing Marvin's DB
+# https://github.com/amazingmarvin/MarvinAPI/wiki/Database-Access
+# https://couchdb-python.readthedocs.io/en/latest/
+# https://cloud.ibm.com/apidocs/cloudant?code=python
+# https://cloud.ibm.com/docs/Cloudant?topic=Cloudant-query#selector-syntax
+import couchdb
+
+
+app = Flask(__name__)
+CORS(app)
+auth = HTTPTokenAuth(scheme='Bearer')
+
+
+# First we load a bunch of settings from the environment
+access_tokens = json.loads(os.environ.get("ACCESS_TOKENS_LIST", '[]'))
+couchdb_username = os.environ.get("COUCHDB_USERNAME", "")
+couchdb_password = os.environ.get("COUCHDB_PASSWORD", "")
+couchdb_hostname = os.environ.get("COUCHDB_HOSTNAME", "")
+couchdb_database = os.environ.get("COUCHDB_DATABASE", "")
+sort_list = json.loads(os.environ.get("SORT_LIST", '[]'))
+UPDATE_TIMEOUT_MILLISECONDS = os.environ.get("UPDATE_TIMEOUT_MILLISECONDS", 5000)
+time_between_queries = 1/os.environ.get("QPS_RATE_LIMIT", 1)
+
+
+# This function lets us globally rate limit calls to the Amazing Marvin backend database
+last_query_time = 0
+def rate_limit(func):
+    global last_query_time
+    time.sleep(max(0, last_query_time + time_between_queries - time.time()))
+    last_query_time = time.time()
+    return func()
+
+
+# Setup the database, and initialize the local copy of the database
+if couchdb_username and couchdb_password and couchdb_hostname and couchdb_database:
+    server = couchdb.Server(f"https://{couchdb_username}:{couchdb_password}@{couchdb_hostname}")
+    db = server[couchdb_database]    
+    last_seq = rate_limit(lambda: db.info().get('update_seq'))
+    local_db = dict([(item.id, item) for item in rate_limit(lambda: db.find({"selector": {"$or": [{"db": "Tasks"}, {"db": "Categories"}]}}))])
+else:
+    db = None
+    last_seq = ""
+    local_db = dict()
+
+
+# Update our local_db given a list of changes that returned from db.changes(...)
+def update_db(changes):
+    global local_db
+    local_db.update([(item.id, item) for item in rate_limit(lambda: db.find({"selector": {"$or": [{"_id": id} for id in set([change['id'] for change in changes if not change.get('deleted', False)])]}})) if item['db']=="Tasks" or item["db"]=="Categories"])
+    for id_to_delete in set([change['id'] for change in changes if change.get("deleted", False) and change['id'] in local_db]):
+        del local_db[id_to_delete]
+
+    
+def label_key(task, options):
+    for label_title in options["labels"]:
+        if label_titles_to_ids[label_title] in task.get('labelIds', []):
+            return options["labels"].index(label_title)
+    if options.get("no_match_last", True):
+        return len(options["labels"])
+    else:
+        return -1
+
+
+label_titles_to_ids = {}
+def label_key_factory(options):
+    global label_titles_to_ids
+    if not label_titles_to_ids:
+        result = rate_limit(lambda: db.get("strategySettings.labels"))
+        if result:
+            label_titles_to_ids = dict([(label['title'], label['_id']) for label in result.get('val', [])])
+    
+    return (lambda task: label_key(task, options))
+
+
+def is_ready(task):
+    ready = (task.get('backburner', False) == False)
+    dependsOn = task.get('dependsOn', {})
+    if isinstance(dependsOn, dict):
+        ready = (ready and all([local_db.get(key, {"done": True}).get('done', False) for key in dependsOn.keys()]))
+    return ready
+
+
+sort_functions = {"field": lambda options: (lambda task: task.get(options["field_name"], options["empty_value"]),
+                                            options.get("reverse", False),
+                                            [options["field_name"]]),
+                  "label": lambda options: (label_key_factory(options),
+                                            options.get("reverse", False),
+                                            ["labelIds"]),
+                  "is_ready": lambda options: (lambda task: is_ready(task),
+                                               options.get("reverse", False),
+                                               ["done", "dependsOn", "backburner"])}
+sort_list.append(("field", {"field_name": "masterRank", "empty_value": 0}))
+processed_sort_list = []
+relevant_fields = set(["parentId"])
+for sort in reversed(sort_list):
+    key, reverse, fields = sort_functions[sort[0]](sort[1])
+    processed_sort_list.append((key, reverse))
+    relevant_fields.update(fields)
+
+
+# edited_task is the one that changed
+# db_updated is a function that takes the edited_task and the changes and returns True if the edit is reflected in the changes or local_db
+def sort_and_update_by_task(edited_task, db_updated, sort_old_project=False, sort_dependencies=False):    
+    # First we check that the edited_task has all the fields we're going to need
+    if not isinstance(edited_task, dict) or not all([field in edited_task.keys() for field in ["_id", "parentId"]]):
+        return {"success": False, "message": "edited_task wasn't a dict or didn't have the required fields"}
+
+    # start by including the current project in the list of tasks to sort
+    parent_ids = set([edited_task["parentId"]])
+
+    if sort_old_project:
+        # This gets the parentId of the task from the list of previous revisions (assuming the parentId wasn't "unassigned", which means inbox)
+        old_parent_id = local_db[edited_task["_id"]]['parentId']
+        if old_parent_id != "unassigned":
+            parent_ids.update(old_parent_id)
+
+    # Next we wait until the database is updated
+    changes = []
+    global last_seq
+    if not db_updated(edited_task, changes):
+        change_feed = rate_limit(lambda: db.changes(since=last_seq, feed="continuous", timeout=UPDATE_TIMEOUT_MILLISECONDS))
+        for change in change_feed:
+            if 'last_seq' in change:
+                last_seq = change['last_seq']
+            else:
+                changes.append(change)
+                if db_updated(edited_task, changes):
+                    last_seq = change['seq']
+                    break
+        global last_query_time
+        last_query_time = time.time()
+        # Download the changes and update our db
+        update_db(changes)
+    # If we're still waiting for the database to be updated, return failure
+    if not db_updated(edited_task, changes):
+        return {"success": False, "message": "timed out waiting for deletion, last_seq = {}".format(last_seq)}
+
+    if sort_dependencies:
+        # This crazy one-liner uses a list comprehension to get all parentIds of tasks for which the edited_task's id is in the task's dependsOn list
+        parent_ids.update([task['parentId'] for task in local_db.values() if edited_task["_id"] in (task.get('dependsOn', {}) if task.get('dependsOn') is not None else {}).keys()])
+
+    return sort_and_update_by_parent_ids(parent_ids)
+
+
+def sort_and_update_by_parent_ids(parent_ids):
+    # This gets all the tasks in all the projects in parent_ids
+    affected_tasks = list([item for item in local_db.values() if item['parentId'] in parent_ids])
+    # We sort per-project, but will update all the changes at once
+    updated_docs = []
+    for parent_id in parent_ids:
+        project_tasks = [task for task in affected_tasks if task['parentId'] == parent_id]
+        # do the sorting
+        for key, reverse in processed_sort_list:
+            project_tasks.sort(key=key, reverse=reverse)
+        # cycle through  all of the tasks by index
+        for i in range(len(project_tasks)):
+            # if its old masterRank doesn't equal it's new index+1 (+1 because masterRank starts at 1, not 0)
+            if project_tasks[i].get('masterRank') != i+1:
+                project_tasks[i]['masterRank'] = i+1
+                updated_docs.append(project_tasks[i])
+    # Update the changed docs on the server
+    result = rate_limit(lambda: db.update(updated_docs))
+    if not all(item[1] for item in result):
+        return {"success": False, "message": "Error updating docs in database"}
+    return {"success": True, "message": "Updated {}/{} docs, last_seq = {}".format(len(updated_docs), len(affected_tasks), last_seq)}    
+
+
+@auth.verify_token
+def verify_token(token):
+    app.logger.debug("Trying to verify with token {}".format(token))
+    return token in access_tokens
+
+
+@app.before_request
+def check_db_connection():
+    if not db:
+        return {"success": False, "message": "db connection failed"}
+
+
+@app.route('/<string:access_token>/sortAll')
+def sortAll(access_token):
+    global last_seq
+    if verify_token(access_token):
+        changes = rate_limit(lambda: db.changes(since=last_seq))
+        update_db(changes["results"])
+        last_seq = changes["last_seq"]
+        project_ids = [item.id for item in local_db.values() if item['db'] == "Categories"] #[project["_id"] for project in db.find({"selector": {"db": "Categories"}})]
+        result =  sort_and_update_by_parent_ids(project_ids)
+        if result["success"]:
+            return(send_from_directory(".", "close.html"))
+        else:
+            return result
+    else:
+        abort(404)    
+
+    
+@app.route('/edit', methods=['POST'])
+@auth.login_required
+def edit():
+    global last_seq
+    edited_task = request.get_json(silent=True)
+    updated_fields = edited_task.get("setter", {}).keys()
+    if any([updated_field in relevant_fields for updated_field in updated_fields]):
+        # if done changed, need to sort current project and all tasks which depend on task
+        # if parentId changed, need to sort old project and current project
+        return sort_and_update_by_task(edited_task,
+                                       # If we're waiting for the update, then the new revision should either already be in the local_db, or the id and rev should be in the change list
+                                       lambda task, changes: task.get("_rev") == local_db.get(task["_id"], {}).get("_rev") or (task["_id"], [task.get("_rev")]) in [(change['id'], [item['rev'] for item in change['changes']]) for change in changes], #task.get("_rev") not in [task.rev for task in revisions],
+                                       sort_old_project=("parentId" in updated_fields),
+                                       sort_dependencies=("done" in updated_fields))
+    return {"success": True, "message": "No relevant updates, last_seq = {}".format(last_seq)}
+
+
+@app.route('/add', methods=['POST'])
+@auth.login_required
+def add():
+    edited_task=request.get_json(silent=True)
+    # if current project is not inbox (i.e. "unassigned"), sort current project    
+    if edited_task.get("parentId", "unassigned") != "unassigned":
+        return sort_and_update_by_task(edited_task,
+                                       # If we're waiting for an add, then the doc need only exist
+                                       lambda task, changes: task["_id"] in local_db or task["_id"] in [change["id"] for change in changes]) #lambda task, revisions: len(revisions) == 0)
+            
+
+@app.route('/markDone', methods=['POST'])
+@auth.login_required
+def markDone():
+    edited_task=request.get_json(silent=True)
+    # if done changed, need to sort current project and all tasks which depend on task    
+    return sort_and_update_by_task(edited_task,
+                                   # If we're waiting for the update, then the new revision should either already be in the local_db, or the id and rev should be in the change list
+                                   lambda task, changes: task.get("_rev") == local_db.get(task["_id"], {}).get("_rev") or (task["_id"], [task.get("_rev")]) in [(change['id'], [item['rev'] for item in change['changes']]) for change in changes], #task.get("_rev") not in [task.rev for task in revisions],
+                                   sort_dependencies=True)
+
+
+@app.route('/delete', methods=['POST'])
+@auth.login_required
+def delete():
+    edited_task=request.get_json(silent=True)
+    # if task deleted, need to sort current project and all tasks which depend on task
+    return sort_and_update_by_task(edited_task,
+                                   # If we're waiting for a deletion, either the task should no longer exist, or deleted: True should be in one of the changes
+                                   lambda task, changes: task["_id"] not in local_db or task["_id"] in [change['id'] for change in changes if change.get("deleted", False)], #lambda task, revisions: len(revisions) > 0,
+                                   sort_dependencies=True)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
